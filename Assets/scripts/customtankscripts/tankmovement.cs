64027823719ffa38f29a9bcef57ea09a5b43240f
mhagstrom <mhagstrom86293@uat.edu>
2024-09-13 15:04:10 -0700

    customtank moves and aims

--- Assets/scripts/customtankscripts/tankmovement.cs
+++ Assets/scripts/customtankscripts/tankmovement.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class tankmovement : MonoBehaviour
 {
     private Rigidbody rb;
 
+    //if it has no reason to be public set it to private, if you still need it exposed in the inspector set it as a serializefield private so only this script can use it, but its values are still editable in unity
     [Header("Movement Data")]
-    public float moveSpeed;
-    public float rotationSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
     private float verticalInput;
     private float horizontalInput;
+    [Header("Aim Data")]
+    [SerializeField] private LayerMask whatIsAimMask;
+    [SerializeField] private Transform aimSphere;
+
+    [Header("Turret Data")]
+    [SerializeField] private Transform tankturret;
+    [SerializeField] private float turretRotationSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +29,12 @@ public class tankmovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        UpdateAim();
+        CheckInputs();
+    }
+
+    private void CheckInputs()
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
@@ -31,9 +46,42 @@ public class tankmovement : MonoBehaviour
     //Timestamp of update rate found in project settings of unity > time
     private void FixedUpdate()
     {
+        ApplyMovement();
+        ApplyHullRotation();
+        ApplyTurretRotation();
+    }
+
+    private void ApplyTurretRotation()
+    {
+        Vector3 direction = aimSphere.position - tankturret.position;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        tankturret.rotation = Quaternion.RotateTowards(tankturret.rotation, targetRotation, turretRotationSpeed);
+
+        //tankturret.LookAt(aimSphere);
+    }
+
+    private void ApplyHullRotation()
+    {
+        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+    }
+
+    private void ApplyMovement()
+    {
         Vector3 movement = transform.forward * moveSpeed * verticalInput;
         rb.velocity = movement;
+    }
 
-        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+    private void UpdateAim()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, whatIsAimMask))
+        {
+            aimSphere.position = hit.point;
+        }
     }
 }
